834855909fed9073c08c09701d60df452d7df00e
Aaron Harvey <harvnlenny@gmail.com>
2015-06-09 15:21:30 -0400

    login works and you need to login to view posts/ make comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email_address])
+    user = User.find_by_email_address(params[:email_address])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 
 end
