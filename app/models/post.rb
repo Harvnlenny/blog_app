c9dd5d44d9af132e358d27bfaec1f7963c6b057a
Aaron Harvey <harvnlenny@gmail.com>
2015-06-10 18:49:57 -0400

    added tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   has_many :blogs
+  has_many :taggings
+  has_many :tags, :through => :taggings
   validates :title, presence: true
   validates :body, presence: true
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+ 
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 end
