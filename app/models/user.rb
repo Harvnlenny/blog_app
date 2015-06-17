20e66fb942c7512715becc2d60239d16fe879a24
Aaron Harvey <harvnlenny@gmail.com>
2015-06-17 08:52:22 -0400

    Yay\!

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ActiveRecord::Base
 
   def self.authorize_with(auth)
     where(provider: auth["provider"], uid: auth["uid"]).first_or_create.tap do |u|
-      u.name         = auth.info.name
-      u.email        = auth.info.email
-      u.nickname     = auth.info.nickname
-      u.access_token = auth.credentials.token
+      u.name          = auth.info.name
+      u.email_address = auth.info.email
+      u.nickname      = auth.info.nickname
+      u.access_token  = auth.credentials.token
       u.save!
     end
   end
