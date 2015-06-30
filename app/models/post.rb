530408e8e5d13818a79224d7f221b35413861e42
Aaron Harvey <harvnlenny@gmail.com>
2015-06-30 14:04:41 -0400

    Active Support Concern

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,12 @@
 class Post < ActiveRecord::Base
+  include Taggable
   has_many :comments
   belongs_to :user
   has_many :blogs
-  has_many :taggings
-  has_many :tags, :through => :taggings
+  
   validates :title, presence: true
   validates :body, presence: true
   mount_uploader :pic, PicUploader
   
-  def all_tags=(names)
-    self.tags = names.split(",").map do |name|
-      Tag.where(name: name.strip).first_or_create!
-    end
-  end
- 
-  def all_tags
-    self.tags.map(&:name).join(", ")
-  end
+  
 end
